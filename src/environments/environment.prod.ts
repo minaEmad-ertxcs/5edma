0ffee2a398d8af256aef8e5ef0072731911eeb88
mina1234emad-admin <mina.emadnaguib@vodafone.com>
2025-11-17 23:58:57 +0200

    build 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'https://minaemad-ertxcs.github.io'
+  apiUrl: 'https://minaemad-ertxcs.github.io/khedma'
 };
